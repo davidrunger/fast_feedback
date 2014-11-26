73475462cd79c415b6f0e197daac83aaf5c7e013
David Runger <daverunger@gmail.com>
2014-11-25 18:01:13 -0800

    recommit without chrome break point?

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
   validates :title, presence: true
   has_many :answers
-  accepts_nested_attributes_for :answers
+  accepts_nested_attributes_for :answers #
 end
