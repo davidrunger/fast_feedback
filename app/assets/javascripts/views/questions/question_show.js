a6ce2169ae93e07218d943c3200807091390e6b3
David Runger <daverunger@gmail.com>
2014-11-27 23:29:36 -0800

    add chart demo/test

--- app/assets/javascripts/views/questions/question_show.js
+++ app/assets/javascripts/views/questions/question_show.js
@@ -13,7 +13,34 @@ FastFeedback.Views.QuestionShow = Backbone.CompositeView.extend({
       this.addSubview('.responses', answerResponses.render());
     }.bind(this))
     this.attachSubviews();
+    this.renderChart();
     return this;
+  },
+
+  renderChart: function () {
+    $('#results-chart').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
   },
 
   template: JST['questions/question_show']
