72e13d2e3906639129aedd412ab762e360d07381
David Runger <daverunger@gmail.com>
2014-11-27 22:36:33 -0800

    add highcharts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require jquery_ujs
 //= require jquery.serializejson.min
 //= require jquery.cycle.lite
+//= require highcharts
 //= require underscore
 //= require backbone
 //= require fast_feedback
