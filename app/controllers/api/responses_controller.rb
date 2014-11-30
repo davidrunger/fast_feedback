0602a4a6d413266820d67ed8eb1eccecf8cc0e35
David Runger <daverunger@gmail.com>
2014-11-29 17:16:31 -0800

    checkin progress with nexmo sms-ing

--- app/controllers/api/responses_controller.rb
+++ app/controllers/api/responses_controller.rb
@@ -2,22 +2,21 @@ class Api::ResponsesController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
+    response = Response.new(
+      answer_id: Answer.where(sms_code: params[:text]).select('id').first.id,
+      answerer_id: params[:msisdn]
+    )
+    if response.save
+      Pusher.app_id = ENV['PUSHER_APP_ID']
+      Pusher.key = ENV['PUSHER_KEY']
+      Pusher.secret = ENV['PUSHER_SECRET']
+      Pusher.trigger('response-updates', 'response-event', {:message => 'hello world'})
+    end
     render json: nil, status: :ok
-    # response = Response.new(response_params)
-    # response.answerer_id = 'djr2dd2ac'
-    # if response.save
-    #   Pusher.app_id = ENV['PUSHER_APP_ID']
-    #   Pusher.key = ENV['PUSHER_KEY']
-    #   Pusher.secret = ENV['PUSHER_SECRET']
-    #   Pusher.trigger('response-updates', 'response-event', {:message => 'hello world'})
-    #   render json: response
-    # else
-    #   render json: nil
-    # end
   end
 
   private
   def response_params
-    params.require(:response).permit(:answer_id)
+    params.permit(:msisdn, :text)
   end
 end
