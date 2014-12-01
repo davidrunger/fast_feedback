dd8d8c0a2d9451ef7756ccac03dedf74a3293f33
David Runger <daverunger@gmail.com>
2014-12-01 00:15:53 -0800

    destroy current session with json request

--- app/assets/javascripts/views/static/header.js
+++ app/assets/javascripts/views/static/header.js
@@ -1,4 +1,8 @@
 FastFeedback.Views.Header = Backbone.CompositeView.extend({
+  events: {
+    'click .sign-out': 'signOut'
+  },
+
   initialize: function (options) {
     this.current_user = options.current_user;
     this.listenTo(this.current_user, 'sync', this.render);
@@ -8,6 +12,19 @@ FastFeedback.Views.Header = Backbone.CompositeView.extend({
     var content = this.template({ current_user: this.current_user });
     this.$el.html(content);
     return this;
+  },
+
+  signOut: function (event) {
+    event.preventDefault();
+    $.ajax({
+        url: 'api/current_user',
+        type: 'DELETE',
+        dataType: 'json',
+        success: function(result) {
+          debugger
+          Backbone.history.navigate('#/');
+        }
+    });
   },
 
   template: JST['static/header']
--- app/controllers/api/current_users_controller.rb
+++ app/controllers/api/current_users_controller.rb
@@ -1,5 +1,10 @@
 class Api::CurrentUsersController < ApplicationController
+  def logout
+    logout!
+    render json: {'status' => 'successfully logged out'}, status: :ok
+  end
+
   def show
-    render 'current_user'
+    logged_in? ? (render 'current_user') : (render json: nil)
   end
 end
