0736c37b3bf1af6b961d6330fc5f82d9aacbafbc
davidrunger <daverunger@gmail.com>
2015-03-20 03:09:35 -0700

    upgrade to rails 4.2 (and remove rack-zippy)

--- Gemfile
+++ Gemfile
@@ -1,30 +1,34 @@
 source 'https://rubygems.org'
 
-gem 'rails', '4.1.7'
+gem 'rails', '4.2.0'
 gem 'pg'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
-gem 'sdoc', '~> 0.4.0',          group: :doc
-gem 'spring',        group: :development
 gem 'bcrypt', '~> 3.1.7'
-gem 'pry-byebug', group: [:development, :test]
-gem 'byebug', group: [:development, :test]
-gem 'rails_12factor', group: :production
 gem 'newrelic_rpm'
 gem 'bootstrap-sass', '~> 3.3.1'
 gem 'sass-rails', '>= 3.2'
 gem 'autoprefixer-rails'
 gem 'backbone-on-rails'
-gem "figaro"
+gem 'figaro'
 gem 'pusher', '~> 0.14.2'
 gem 'ejs'
-gem 'rack-zippy'
 gem 'seed_dump'
 
 group :development do
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'annotate'
+  gem 'spring'
+end
+
+group :development, :test do
+  gem 'pry-byebug'
+  gem 'byebug'
+end
+
+group :production do
+  gem 'rails_12factor'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,56 +16,46 @@ ActiveRecord::Schema.define(version: 20141205191923) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "answers", force: true do |t|
-    t.integer  "question_id", null: false
-    t.text     "text",        null: false
-    t.string   "pic_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "ord"
+  create_table "answers", force: :cascade do |t|
+    t.integer "question_id",             null: false
+    t.text    "text",                    null: false
+    t.string  "pic_url",     limit: 255
+    t.integer "ord"
   end
 
   add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
 
-  create_table "questions", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "survey_id"
-    t.string   "chart_type"
-    t.string   "pic_url"
-    t.text     "title",      null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "ord"
+  create_table "questions", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "survey_id"
+    t.string  "chart_type", limit: 255
+    t.string  "pic_url",    limit: 255
+    t.text    "title",                  null: false
+    t.integer "ord"
   end
 
   add_index "questions", ["survey_id"], name: "index_questions_on_survey_id", using: :btree
   add_index "questions", ["user_id"], name: "index_questions_on_user_id", using: :btree
 
-  create_table "responses", force: true do |t|
-    t.integer  "answer_id",   null: false
-    t.string   "answerer_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "responses", force: :cascade do |t|
+    t.integer "answer_id",               null: false
+    t.string  "answerer_id", limit: 255
   end
 
   add_index "responses", ["answer_id"], name: "index_responses_on_answer_id", using: :btree
 
-  create_table "surveys", force: true do |t|
-    t.integer  "user_id",                           null: false
-    t.string   "title",                             null: false
-    t.integer  "default_num_questions", default: 2
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "surveys", force: :cascade do |t|
+    t.integer "user_id",                                       null: false
+    t.string  "title",                 limit: 255,             null: false
+    t.integer "default_num_questions",             default: 2
   end
 
   add_index "surveys", ["user_id"], name: "index_surveys_on_user_id", using: :btree
 
-  create_table "users", force: true do |t|
-    t.string   "email",           null: false
-    t.string   "session_token",   null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "users", force: :cascade do |t|
+    t.string "email",           limit: 255, null: false
+    t.string "session_token",   limit: 255, null: false
+    t.string "password_digest", limit: 255, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
